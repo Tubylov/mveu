67fc1d67637c1083fee26b95c6fc6a05cbd6d280
Ilya <ilya.tubylov@yandex.ru>
2023-12-28 23:14:49 +0400

    Удален комментарий

--- backend/index.js
+++ backend/index.js
@@ -49,17 +49,6 @@ app.post('/login', async (req, res) => {
 })
 
 app.get('/products', async (req, res) => {
-
-    /*const products = [
-        { id: 1, header: 'Товар 1', price: 120 },
-        { id: 2, header: 'Товар 2', price: 3850 },
-        { id: 3, header: 'Товар 3', price: 570 },
-        { id: 4, header: 'Товар 4', price: 14360 },
-        { id: 5, header: 'Товар 5', price: 98 },
-        { id: 6, header: 'Товар 6', price: 734 },
-        { id: 7, header: 'Товар 7', price: 198 },
-        { id: 8, header: 'Товар 8', price: 334 }
-    ]*/
     
     const products = await Product.find()
 
